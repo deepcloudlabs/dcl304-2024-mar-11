336ff7bd6191114ef6b52afef36e517ab07dd02e
deepcloudlabs <deepcloudlabs@gmail.com>
2024-03-12 10:38:00 +0300

    Update exercise05.js

--- study-js/oop/exercise05.js
+++ study-js/oop/exercise05.js
@@ -4,13 +4,22 @@ class Employee {
         this.fullname = fullname;
         this.salary = salary;
         this.iban = iban;
+//        this.increaseSalary = this.increaseSalary.bind(this);
+//        this.sayHello = this.sayHello.bind(this);
     }
 
-    increaseSalary(rate){
+    // this.increaseSalary = this.increaseSalary.bind(this); : automatically binds to this
+    increaseSalary = (rate) => { // lambda expression
         this.salary = this.salary * (1.0 + rate / 100);
     }
 
-    sayHello(){
+    // this.sayHello = this.sayHello.bind(this); : automatically binds to this
+    sayHello = () => { // lambda expression
+        // console.table(this);
         console.log(`Hello, ${this.fullname}!`);
     }
 }
+
+let jack = new Employee("11111111110", "jack bauer", 100_000, "tr1");
+jack.sayHello();
+setInterval( jack.sayHello, 3_000);
