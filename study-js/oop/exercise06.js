53e818a011ec576be5a3bbdc279e6fd2e00ffe1e
deepcloudlabs <deepcloudlabs@gmail.com>
2024-03-12 11:13:30 +0300

    Create exercise06.js

---
+++ study-js/oop/exercise06.js
@@ -0,0 +1,28 @@
+class Account {
+    // private members
+    // information hiding!
+    #iban;
+    #balance;
+    constructor(iban,balance) {
+        this.#iban = iban;
+        this.#balance = balance;
+    }
+    balance = () => {
+        return this.#balance;
+    }
+    deposit = (amount) => {
+        if (amount <= 0) throw "You must provide a positive amount";
+        this.#balance += amount;
+    }
+    withdraw = (amount) => {
+        if (amount <= 0) throw "You must provide a positive amount";
+        if (amount > this.#balance) throw `You must provide an amount less than or equal to balance (${this.balance})`;
+        this.#balance -= amount;
+    }
+}
+
+let acc1 = new Account("tr1", 100_000);
+acc1.deposit(25_000); // 125_000
+acc1.withdraw(112_500); // 12_500
+// acc1.#balance -= 100_000_000;
+console.log(acc1.balance());
