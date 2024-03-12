69d04dba3c6951b76b8e1bcd48fbb30c3e5be854
deepcloudlabs <deepcloudlabs@gmail.com>
2024-03-12 14:05:01 +0300

    Create exercise05.js

---
+++ study-js/functional-programming/exercise05.js
@@ -0,0 +1,9 @@
+employees = [
+    {"identity": "1", "name": "jack",salary: 100_000, departments: ["IT", "SALES"]},
+    {"identity": "2", "name": "kate",salary: 600_000, departments: ["FINANCE"]},
+    {"identity": "3", "name": "james",salary: 800_000, departments: ["IT"]},
+    {"identity": "4", "name": "ben",salary: 400_000, departments: ["SALES"]},
+    {"identity": "5", "name": "jin",salary: 500_000, departments: ["IT", "FINANCE", "SALES"]}
+]
+// find distinct departments
+let departments = employees.map(???).???
