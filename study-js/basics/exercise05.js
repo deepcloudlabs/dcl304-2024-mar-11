b21411b9bef3dbb1b121e89b595f2a6a841fbe63
deepcloudlabs <deepcloudlabs@gmail.com>
2024-03-12 14:19:15 +0300

    Create exercise05.js

---
+++ study-js/basics/exercise05.js
@@ -0,0 +1,14 @@
+let numbers = new Set()
+numbers.add(4);
+numbers.add(4);
+numbers.add(8);
+numbers.add(8);
+numbers.add(8);
+numbers.add(14);
+numbers.add(14);
+numbers.add(14);
+numbers.add(14);
+numbers.add(14);
+numbers.add(14);
+numbers.add(14);
+console.log(numbers)
