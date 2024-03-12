2e79585ad50cef82807d1ddbf608cad43676e6a5
deepcloudlabs <deepcloudlabs@gmail.com>
2024-03-12 15:33:17 +0300

    Create exercise02.js

---
+++ study-js/object/exercise02.js
@@ -0,0 +1,20 @@
+const circle2 = {
+    x: 0,
+    y: 0,
+    radius: 100,
+    draw: {
+        thickness: 2,
+        color: "red",
+        style: "solid"
+    }
+}
+
+
+const x2 = circle2.x;
+const y2 = circle2.y;
+const radius2 = circle2.radius;
+
+let {x,y,radius} = circle2;
+console.log(x)
+console.log(y)
+console.log(radius)
