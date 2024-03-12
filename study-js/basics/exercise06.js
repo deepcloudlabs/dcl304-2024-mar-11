e35c01ac59f9df8a355a68ff10fdf3813611886c
deepcloudlabs <deepcloudlabs@gmail.com>
2024-03-12 14:29:14 +0300

    Create exercise06.js

---
+++ study-js/basics/exercise06.js
@@ -0,0 +1,5 @@
+areaCodes = { "ankara": 312}
+// areaCodes["ankara"] = 312
+areaCodes["istanbul"] = {"anadolu": 216, "avrupa": 212}
+console.log(areaCodes["istanbul"]["avrupa"])
+console.log(areaCodes["ankara"])
