460c83de8f4a8c835dad07c93db8454a8cf7a570
deepcloudlabs <deepcloudlabs@gmail.com>
2024-03-12 14:19:21 +0300

    Update exercise05.js

--- study-js/functional-programming/exercise05.js
+++ study-js/functional-programming/exercise05.js
@@ -2,8 +2,13 @@ employees = [
     {"identity": "1", "name": "jack",salary: 100_000, departments: ["IT", "SALES"]},
     {"identity": "2", "name": "kate",salary: 600_000, departments: ["FINANCE"]},
     {"identity": "3", "name": "james",salary: 800_000, departments: ["IT"]},
-    {"identity": "4", "name": "ben",salary: 400_000, departments: ["SALES"]},
+    {"identity": "4", "name": "ben",salary: 400_000, departments: ["SALES", "HR"]},
     {"identity": "5", "name": "jin",salary: 500_000, departments: ["IT", "FINANCE", "SALES"]}
 ]
 // find distinct departments
-let departments = employees.map(???).???
+let departments = employees.map(employee => employee.departments)
+                           .reduce((distinct_depts,depts)=>{
+                               depts.forEach(d => distinct_depts.add(d));
+                               return distinct_depts;
+                           }, new Set());
+console.log(departments)
