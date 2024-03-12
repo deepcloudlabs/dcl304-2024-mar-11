45fc3d075839a5ed7f1e4b95303d9a3454493ac4
deepcloudlabs <deepcloudlabs@gmail.com>
2024-03-12 13:45:28 +0300

    Create exercise04.js

---
+++ study-js/functional-programming/exercise04.js
@@ -0,0 +1,8 @@
+employees = [
+    {"identity": "1", "name": "jack",salary: 100_000, departments: ["IT", "SALES"]},
+    {"identity": "2", "name": "kate",salary: 600_000, departments: ["FINANCE"]},
+    {"identity": "3", "name": "james",salary: 800_000, departments: ["IT"]},
+    {"identity": "4", "name": "ben",salary: 400_000, departments: ["SALES"]},
+    {"identity": "5", "name": "jin",salary: 500_000, departments: ["IT", "FINANCE"]}
+]
+employees.sort(???).forEach(employee => console.log(employee))
